364839cdaa797586eb7a4faf62c4d1f73497dcfa
Pelle Bäckström <39518504+Pelle1994@users.noreply.github.com>
2021-10-19 13:54:48 +0200

    Add files via upload

---
+++ Controllers/AdsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ServiceHeroWebApi.Interfaces;
+using ServiceHeroWebApi.Models;
+using ServiceHeroWebApi.Services;
+
+namespace ServiceHeroWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AdsController : ControllerBase
+    {
+
+        private readonly HttpClient _client;
+        private readonly IService _service;
+
+        public AdsController(IHttpClientFactory factory, IService service)
+        {
+            _client = factory.CreateClient("Jobstream");
+            _service = service;
+        }
+
+
+        // GET: api/v1/<AdsController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+
+
+            string date = DateTime.Today.AddMonths(-1).ToShortDateString();
+
+            string endpoint =
+                "/stream?date=" + date + "&occupation-concept-id=ScKy_FHB_7wT";
+
+            var result = await _client.GetAsync(endpoint);
+
+            var resultString = await result.Content.ReadAsStringAsync();
+
+            var list = JsonConvert.DeserializeObject<List<JobAdDto>>(resultString);
+
+            await _service.AddNewAds(list);
+
+            return Ok();
+        }
+    }
+}
---
+++ Data/AdContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ServiceHeroWebApi.Models;
+
+namespace ServiceHeroWebApi.Data
+{
+    public class AdContext : DbContext
+    {
+        public AdContext(DbContextOptions<AdContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+        //entities
+        public DbSet<JobAd> JobAds { get; set; }
+
+    }
+}
